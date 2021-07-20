4d2c729aafb95c3eb26cec7d18e0cbfbc978ad1f
onlyshow <onlyxiaoshow@gmail.com>
2021-07-20 10:12:28 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,193 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'onlyshow/dtmcli-php',
+  ),
+  'versions' => 
+  array (
+    'ezimuel/guzzlestreams' => 
+    array (
+      'pretty_version' => '3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'abe3791d231167f14eb80d413420d1eab91163a8',
+    ),
+    'ezimuel/ringphp' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b78f89d8e0bb9e380046c31adfa40347e9f663b',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '6.5.5',
+      'version' => '6.5.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d4290de1cfd701f38099ef7e183b64b4b7b0c5e',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.8.2',
+      'version' => '1.8.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc960a912984efb74d0a90222870c72c87f10c91',
+    ),
+    'mix/guzzle' => 
+    array (
+      'pretty_version' => 'v2.2.16',
+      'version' => '2.2.16.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4db6ddb6c5be7117f138b7e8f7191a0a7744c926',
+    ),
+    'mix/http-message' => 
+    array (
+      'pretty_version' => 'v3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6dc597d90064ff7f1612622d1161afaa7d900262',
+    ),
+    'mix/vega' => 
+    array (
+      'pretty_version' => 'v3.0.6',
+      'version' => '3.0.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408c6bb1999eff9b7d341800682b61934cd8464b',
+    ),
+    'mix/view' => 
+    array (
+      'pretty_version' => 'v3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '96ef3570803a86d2b17fc57f89643b6bb114bde6',
+    ),
+    'nikic/fast-route' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+    ),
+    'onlyshow/dtmcli-php' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'react/promise' => 
+    array (
+      'pretty_version' => 'v2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3cff96a19736714524ca0dd1d4130de73dbbbc4',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65bd267525e82759e7d8c4e8ceea44f398838e65',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8590a5f561694770bdcd3f9b5c69dde6945028e8',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a142215a36a3888e30d0a9eeea9766764e96976',
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.19',
+      'version' => '4.0.19.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af6025976fba817eeb4d5fbf8d0c1059a5819da3',
+    ),
+  ),
+);
---
+++ vendor/mix/vega/src/SubRouter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Mix\Vega;
+
+/**
+ * Class SubRouter
+ * @package Mix\Vega
+ */
+class SubRouter
+{
+
+    /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * @var Engine
+     */
+    protected $engine;
+
+    /**
+     * Subrouter constructor.
+     * @param string $prefix
+     * @param Engine $engine
+     */
+    public function __construct(string $prefix, Engine $engine)
+    {
+        $this->prefix = $prefix;
+        $this->engine = $engine;
+    }
+
+    /**
+     * @param string $path
+     * @param \Closure ...$handlers
+     * @return Route
+     * @deprecated
+     */
+    public function handleF(string $path, \Closure ...$handlers): Route
+    {
+        return $this->handleFunc($path, ... $handlers);
+    }
+
+    /**
+     * @param string $path
+     * @param callable ...$handlers
+     * @return Route
+     * @deprecated
+     */
+    public function handleC(string $path, callable ...$handlers): Route
+    {
+        return $this->handleCall($path, ... $handlers);
+    }
+
+    /**
+     * @param string $path
+     * @param \Closure ...$handlers
+     * @return Route
+     */
+    public function handleFunc(string $path, \Closure ...$handlers): Route
+    {
+        return $this->engine->handleFunc($this->prefix . $path, ...$handlers);
+    }
+
+    /**
+     * @param string $path
+     * @param callable ...$handlers
+     * @return Route
+     */
+    public function handleCall(string $path, callable ...$handlers): Route
+    {
+        return $this->engine->handleCall($this->prefix . $path, ...$handlers);
+    }
+
+}
